f09a4db6d621974faf80ca3b00e55f5fe43bca86
= <LiaAufarrahman09@gmail.com>
2022-03-17 01:02:34 +0700

    Update DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([HomeSeeder::class]);
         $this->call([SuplierSeeder::class]);
+        $this->call([PelangganSeeder::class]);
 
         // \App\Models\User::factory(10)->create();
     }
