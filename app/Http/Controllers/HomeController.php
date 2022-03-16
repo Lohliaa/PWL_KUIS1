bc7a0e6588fcc03827f48b404463a1595164474a
= <LiaAufarrahman09@gmail.com>
2022-03-16 19:45:26 +0700

    Update Home Controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Home;
 
 class HomeController extends Controller
 {
     public function index(){
-        $home = Home::all();
+        // $home = Home::all();
         return view('index')
-        ->with('title','Beranda')
-        ->with('Home','$home');
+        ->with('title','Beranda');
+        // ->with('Home','$home');
     }
 }
